8a945ddce4fb5f629c3d55f50b4065d2e0289fb0
HenDeane <henry.deane@ou.ac.uk>
2026-01-13 12:24:40 +0000

    HD updated Main.java to produce a random prime number between 2 and 997

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
 public class Main {
 
-   public int generatePrime() {
-       return 13;
-   }
+	public int generatePrime() {
+		int randomNum = (int)(Math.random() * 1001); // 0 to 1000
+		int primeNum = 3;
+		boolean isNotPrime = true;
+		while (isNotPrime) {
+			for (int i = 2; i < randomNum; i++) {
+				if (randomNum % i == 0) {
+					randomNum = (int)(Math.random() * 1001); // 0 to 1000
+				else {
+					primeNum = randomNum;
+					isNotPrime = false;
+				}
+			}
+		}
+		return primeNum;
+	}
 
-   public static void main(String... args) {
-      Main primeGenerator = new Main();
-      System.out.println(primeGenerator.generatePrime());
-  }
+	public static void main(String... args) {
+		Main primeGenerator = new Main();
+		System.out.println(primeGenerator.generatePrime());
+	}
 
 }
