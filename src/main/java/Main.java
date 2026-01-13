f53b813cad5a8749dff50a71805836a634ea6a25
HenDeane <henry.deane@ou.ac.uk>
2026-01-13 13:20:48 +0000

    HD modified Main.java to generated prime btween 2 and 997

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
 	public int generatePrime() {
-		int randomNum = (int)(Math.random() * 998 + 3); // 2 to 1000
+		int randomNum = (int)(Math.random() * 999 + 2); // 2 to 1000
 		boolean isNotPrime = true;
     
 		while (isNotPrime) {
 			isNotPrime = false;
 			for (int i = 2; i <= Math.sqrt(randomNum); i++) {
 				if (randomNum % i == 0) {
-					randomNum = (int)(Math.random() * 998 + 3);
+					randomNum = (int)(Math.random() * 999 + 2);
 					isNotPrime = true;
 					break; // Exit the for loop and restart checking the new number
 				}
